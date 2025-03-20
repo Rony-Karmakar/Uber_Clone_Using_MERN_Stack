fd0a4731fed821576052b5bb69dee2b87a6b3ef8
Rony Karmakar <ronykarmakar437@gmail.com>
2025-03-20 15:46:18 +0530

    Captain Register is added

--- Backend/app.js
+++ Backend/app.js
@@ -3,19 +3,23 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const cookieparser = require('cookie-parser');
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes')
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes')
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
 
 app.get('/', (req, res) => {
     res.send("Hello world")
 })
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes)
 
 module.exports = app;
